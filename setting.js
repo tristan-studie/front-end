7e479400fe61c4b087b83b90e7f929877f31f873
Tristan <35732585+tristanaarden@users.noreply.github.com>
2020-06-26 10:53:52 +0200

    #7 + #8
    Add user settings for time and amount of photos

--- setting.js
+++ setting.js
@@ -1,20 +1,26 @@
 var timeForm = document.getElementById('time');
+var photoForm = document.getElementById('photo');
 
-if(!localStorage.getItem('time')){
+
+if(!localStorage.getItem('time')|| !localStorage.getItem('photo')){
   populateStorage();
 }else{
   setValue();
 }
 function populateStorage() {
   localStorage.setItem('time', document.getElementById('time').value);
+  localStorage.setItem('photo', document.getElementById('photo').value);
 
   setValue();
 }
 
 function setValue(){
   var currentTime = localStorage.getItem('time');
+  var currentPhoto = localStorage.getItem('photo');
 
   document.getElementById('time').value = currentTime;
+  document.getElementById('photo').value = currentPhoto;
 }
 
 timeForm.onchange = populateStorage;
+photoForm.onchange = populateStorage;
