5381f41c0f6e583470acf998b76ca59b5329e764
Tristan <35732585+tristanaarden@users.noreply.github.com>
2020-09-17 13:10:03 +0200

    Add themeForm to settings

--- setting.js
+++ setting.js
@@ -1,8 +1,9 @@
 var timeForm = document.getElementById('time');
 var photoForm = document.getElementById('photo');
+var themeForm = document.getElementById('themes');
 
 
-if(!localStorage.getItem('time')|| !localStorage.getItem('photo')){
+if(!localStorage.getItem('time')|| !localStorage.getItem('photo') || !localStorage.getItem('themes')){
   populateStorage();
 }else{
   setValue();
@@ -10,17 +11,20 @@ if(!localStorage.getItem('time')|| !localStorage.getItem('photo')){
 function populateStorage() {
   localStorage.setItem('time', document.getElementById('time').value);
   localStorage.setItem('photo', document.getElementById('photo').value);
-
+  localStorage.setItem('themes', document.getElementById('themes').value);
   setValue();
 }
 
 function setValue(){
   var currentTime = localStorage.getItem('time');
   var currentPhoto = localStorage.getItem('photo');
+  var currentTheme = localStorage.getItem('themes');
 
   document.getElementById('time').value = currentTime;
   document.getElementById('photo').value = currentPhoto;
+  document.getElementById('themes').value = currentTheme;
 }
 
 timeForm.onchange = populateStorage;
 photoForm.onchange = populateStorage;
+themeForm.onchange = populateStorage;
