55a94930fd851be7c789eb089a73a381fbae3039
Tristan <35732585+tristanaarden@users.noreply.github.com>
2020-09-25 10:42:20 +0200

    Update history.js

--- history.js
+++ history.js
@@ -7,3 +7,6 @@ function showBoard(){
 
 sortScore.onchange = sortOnScore();
 sortDate.onchange = sortOnDate();
+
+
+//On game-end: calculate score-> save score to history with score, date and time->if more than X scores, delete oldest record and insert new score.
