31e156cf7f084a7b9cb3ee5670c3f09e77cc1f3e
Tristan <35732585+tristanaarden@users.noreply.github.com>
2020-09-29 09:47:50 +0200

    Add sortMenu

--- history.js
+++ history.js
@@ -5,8 +5,29 @@ function showBoard(){
 }
 
 
-sortScore.onchange = sortOnScore();
-sortDate.onchange = sortOnDate();
+function sortBoard(){
+  var sortsetting = document.getElementById('sortOptions').value;
+  switch (sortsetting) {
+    case date asc:
+
+      break;
+    case date desc:
+
+      break;
+    case score asc:
+
+      break;
+    case score desc:
+
+      break;
+  default:
+
+}
+}
+
+var sortForm = document.getElementById('sortOptions');
+
+
 
 
 //On game-end: calculate score-> save score to history with score, date and time->if more than X scores, delete oldest record and insert new score.
