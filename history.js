38d4d92758ad71693c180d08a4899c54f7d2d055
Tristan <35732585+tristanaarden@users.noreply.github.com>
2020-09-29 11:56:25 +0200

    #10 Add sort
    Sort on date or score

--- history.js
+++ history.js
@@ -5,8 +5,21 @@ function showBoard(){
     alert('no scores saved yet. Use the trainer to save a score.');
     window.location.href= "http://localhost/pokebattle/front-end/index.html";
   } else {
-  var scoreboard = localStorage.getItem('scores');
+  var scoreboard = JSON.parse(localStorage.getItem('scores'));
   console.log(scoreboard);
+  document.getElementById('historyContainer').style.display = 'block';
+  document.getElementById('historyContainer').innerHTML = '';
+
+  scoreboard = scoreboard.sort(function(a,b){return a.score + b.score});
+  for(var i =0; i <10; i++){
+    var scoreAmount = scoreboard[i].score;
+    var scoreDate = scoreboard[i].date;
+
+    var entry = document.createElement('li');
+    entry.innerHTML = (typeof(scoreAmount) != 'undefined' ? scoreAmount : "") + " | " + new Date(scoreDate);
+    document.getElementById('historyContainer').appendChild(entry);
+  }
+
 }
 }
 
@@ -15,16 +28,61 @@ function sortBoard(){
   var sortSetting = document.getElementById('sortOptions').value;
   switch (sortSetting) {
     case 'dateAsc':
+    var scoreboard = JSON.parse(localStorage.getItem('scores'));
 
+    document.getElementById('historyContainer').innerHTML = '';
+
+    scoreboard = scoreboard.sort(function(a,b){return Date.parse(a.date) - Date.parse(b.date)});
+    for(var i =0; i <10; i++){
+      var scoreAmount = scoreboard[i].score;
+      var scoreDate = scoreboard[i].date;
+      var entry = document.createElement('li');
+      entry.innerHTML = (typeof(scoreAmount) != 'undefined' ? scoreAmount : "") + " | " + new Date(scoreDate);
+      document.getElementById('historyContainer').appendChild(entry);
+    }
       break;
     case 'dateDesc':
+    var scoreboard = JSON.parse(localStorage.getItem('scores'));
 
+    document.getElementById('historyContainer').innerHTML = '';
+
+    scoreboard = scoreboard.sort(function(a,b){return Date.parse(a.date) + Date.parse(b.date)});
+    for(var i =0; i <10; i++){
+      var scoreAmount = scoreboard[i].score;
+      var scoreDate = scoreboard[i].date;
+      var entry = document.createElement('li');
+      entry.innerHTML = (typeof(scoreAmount) != 'undefined' ? scoreAmount : "") + " | " + new Date(scoreDate);
+      document.getElementById('historyContainer').appendChild(entry);
+    }
       break;
     case 'scoreAsc':
+    var scoreboard = JSON.parse(localStorage.getItem('scores'));
 
+    document.getElementById('historyContainer').innerHTML = '';
+
+    scoreboard = scoreboard.sort(function(a,b){return a.score - b.score});
+    for(var i =0; i <10; i++){
+      var scoreAmount = scoreboard[i].score;
+      var scoreDate = scoreboard[i].date;
+      var entry = document.createElement('li');
+      entry.innerHTML = (typeof(scoreAmount) != 'undefined' ? scoreAmount : "") + " | " + new Date(scoreDate);
+      document.getElementById('historyContainer').appendChild(entry);
+    }
       break;
     case 'scoreDesc':
+    var scoreboard = JSON.parse(localStorage.getItem('scores'));
 
+    document.getElementById('historyContainer').innerHTML = '';
+
+    scoreboard = scoreboard.sort(function(a,b){return a.score + b.score});
+    for(var i =0; i <10; i++){
+      var scoreAmount = scoreboard[i].score;
+      var scoreDate = scoreboard[i].date;
+
+      var entry = document.createElement('li');
+      entry.innerHTML = (typeof(scoreAmount) != 'undefined' ? scoreAmount : "") + " | " + new Date(scoreDate);
+      document.getElementById('historyContainer').appendChild(entry);
+    }
       break;
   default:
 
@@ -32,9 +90,10 @@ function sortBoard(){
 }
 
 sortForm.onchange = sortBoard;
-// localStorage.setItem('scores', [[1, 200],[2, 400]]);
+localStorage.setItem('scores', JSON.stringify([{score: 200, date: "2015-03-25T12:00:00"},{score: 400, date: "2020-06-20T11:00:00"} ]));
 
 showBoard();
+sortBoard();
 
 
 //On game-end: calculate score-> save score to history with score, date and time->if more than X scores, delete oldest record and insert new score.
