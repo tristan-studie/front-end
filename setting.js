16eec13a79863c394c3f6926ab757bcad17aedd8
Tristan <35732585+tristanaarden@users.noreply.github.com>
2020-10-08 11:35:37 +0200

    #15 Als gebruiker kan ik de trainers zo instellen dat personen die de laatste 5 trainingen goed zijn geraden weg worden gelaten uit de training.

--- setting.js
+++ setting.js
@@ -1,16 +1,19 @@
 var timeForm = document.getElementById('time');
 var photoForm = document.getElementById('photo');
 var themeForm = document.getElementById('themes');
-var currentTime, currentPhoto, currentTheme;
+var hideForm = document.getElementById('hideCorrect');
+
+var currentTime, currentPhoto, currentTheme, currentHide;
 
 //Check if settings exist in localStorage, If not => call populateStorage(), Else => call setValue()
-(!localStorage.getItem('time') || !localStorage.getItem('photo') || !localStorage.getItem('themes')) ? populateStorage() : setValue();
+(!localStorage.getItem('time') || !localStorage.getItem('photo') || !localStorage.getItem('themes') || !localStorage.getItem('hideCorrect')) ? populateStorage() : setValue();
 
 //Populate the localStorage with settings
 function populateStorage() {
   localStorage.setItem('time', timeForm.value);
   localStorage.setItem('photo', photoForm.value);
   localStorage.setItem('themes', themeForm.value);
+  localStorage.setItem('hideCorrect', hideForm.value);
   setValue();
 }
 
@@ -19,13 +22,16 @@ function setValue(){
   currentTime = localStorage.getItem('time');
   currentPhoto = localStorage.getItem('photo');
   currentTheme = localStorage.getItem('themes');
+  currentHide = localStorage.getItem('hideCorrect');
 
   timeForm.value = currentTime;
   photoForm.value = currentPhoto;
   themeForm.value = currentTheme;
+  hideForm.value = currentHide;
 }
 
 //When user changes settings, call populateStorage()
 timeForm.onchange = populateStorage;
 photoForm.onchange = populateStorage;
 themeForm.onchange = populateStorage;
+hideForm.onchange = populateStorage;
--- trainer.js
+++ trainer.js
@@ -1,62 +1,59 @@
 var smoelen = [
   ["Wessel Keemink", "images/wessel.jpg" ],
-  ["Maarten van Garderen", "images/maarten.jpg"],
-  ["Thijs ter Horst", "images/thijs.jpg"],
-  ["Luuc van der Ent", "images/luuc.jpg"],
-  ["Gijs Jorna", "images/gijs.jpg"],
-  ["Fabian Plak", "images/fabian.jpg"],
-  ["Ewoud Gommans", "images/ewoud.jpg"],
-  ["Nimir Abdelaziz", "images/nimir.jpg"],
-  ["Gijs van Solkema", "images/gijs2.jpg"],
-  ["Wouter ter Maat", "images/wouter.jpg"],
-  ["Michaël Parkinson", "images/michael.jpg"],
-  ["Robbert Andringa", "images/robbert.jpg"],
-  ["Just Dronkers", "images/just.jpg"]
-];
+ ["Maarten van Garderen", "images/maarten.jpg"],
+ ["Thijs ter Horst", "images/thijs.jpg"],
+ ["Luuc van der Ent", "images/luuc.jpg"],
+ ["Gijs Jorna", "images/gijs.jpg"],
+ ["Fabian Plak", "images/fabian.jpg"],
+ ["Ewoud Gommans", "images/ewoud.jpg"],
+ ["Nimir Abdelaziz", "images/nimir.jpg"],
+ ["Gijs van Solkema", "images/gijs2.jpg"],
+ ["Wouter ter Maat", "images/wouter.jpg"],
+ ["Michaël Parkinson", "images/michael.jpg"],
+ ["Robbert Andringa", "images/robbert.jpg"],
+ ["Just Dronkers", "images/just.jpg"]];
 
 const TIMEAMOUNT = localStorage.getItem('time');
 const PHOTOAMOUNT = localStorage.getItem('photo');
-var totalMoves = 0;
-var correctMoves = 0;
-var selectedName = "";
-var selectedPhoto = "";
-var nameList = [];
-var smoelenArray = [];
-var nameSelected = false;
-var photoSelected = false;
+const HIDECORRECT = localStorage.getItem('hideCorrect');
+const LISTNAMES = document.getElementById('nameList');
+const LISTPHOTOS = document.getElementById('photoList');
+var totalMoves = 0, correctMoves = 0;
+var selectedName = "", selectedPhoto = "";
+var nameList = [], smoelenArray = [];
+var nameSelected = false, photoSelected = false;
+var timeTimer, timeObj, listItem, score, dateNow, people, peoples, scoreboard;
+var names = JSON.parse(localStorage.getItem('names'));
+var nameCount = [];
 
 window.onload = function(){
-
   startTraining();
-  var timeTimer = new TrainerTimer(TIMEAMOUNT);
-  var timeObj = TrainerTimer.parse(TIMEAMOUNT);
+  timeTimer = new TrainerTimer(TIMEAMOUNT);
+  timeObj = TrainerTimer.parse(TIMEAMOUNT);
   format(timeObj.minutes, timeObj.seconds, timeObj.pureseconds);
   timeTimer.onTick(format);
   timeTimer.start();
-function format(minutes, seconds, pureseconds) {
-     minutes = minutes < 10 ? "0" + minutes : minutes;
-     seconds = seconds < 10 ? "0" + seconds : seconds;
-     percentage = 100 + ((pureseconds - TIMEAMOUNT) / TIMEAMOUNT) * 100;
-     document.getElementById('progress').style.width = percentage + "%";
-     if (percentage < 1) {
-       saveScore();
-       alert('de tijd is op!');
-       window.location.href= "http://localhost/pokebattle/front-end/index.html";
-     }
- }
 }
 
-//FROM STACKOVERFLOW-> USED FOR TESTING UNTIL RANDOMIZATION FUNCTION DONE
+function format(minutes, seconds, pureseconds) {
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  percentage = 100 + ((pureseconds - TIMEAMOUNT) / TIMEAMOUNT) * 100;
+  document.getElementById('progress').style.width = percentage + "%";
+  if (percentage <= 0) {
+    saveScore();
+    alert('de tijd is op!');
+    window.location.href= "http://localhost/pokebattle/front-end/index.html";
+  }
+}
+
+//STACKOVERFLOW
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
-
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
+    randomIndex = Math.floor(Math.random() * currentIndex); // Pick a remaining element
     currentIndex -= 1;
-
     // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
@@ -64,120 +61,118 @@ function shuffle(array) {
   }
   return array;
 }
-//END
-function startTraining(){
+//STACKOVERFLOW END
 
-  var listNames = document.getElementById('nameList');
+function calculateNames(){
+  for (let i = 0; i < 5 && i < names.length; i++) { //loop through last 5 games
+    for (let j = 0; j < names[i].length; j++) { //For every name
+      let playerName = names[i][j]['name'];
+        if (JSON.stringify(nameCount).includes(JSON.stringify(playerName))) { //Check if nameCount alredy has a record of the playerName
+          for (let k = 0; k < nameCount.length; k++) { //For all names in nameCount
+            if (JSON.stringify(nameCount[k]['name']) == JSON.stringify(playerName)) { //Check if nameCount name is the same as the playerName
+              (names[i][j]['correct']) ? nameCount[k]['score'] += 1 : nameCount[k]['score'] += 0;
+            }
+          }
+        }else{
+          (names[i][j]['correct']) ? nameCount.unshift({name: playerName, score : 1, photo: names[i][j]['photo']})
+          : nameCount.unshift({name: playerName, score: 0, photo: names[i][j]['photo']});
+        }
+    }
+  }
+for (let i = 0; i < nameCount.length; i++) {
+  if (nameCount[i]['score'] == 5) {
+    for (let j = 0; j < smoelen.length; j++) {
+        if (JSON.stringify(smoelen[j][0]) == JSON.stringify(nameCount[i]['name'])) {
+          smoelen.splice(j ,2);
+
+        }
+    }
+  }
+}
+}
+
+function startTraining(){
+  if (localStorage.getItem('hideCorrect')== 'yes') {
+    calculateNames(smoelen);
+  }
+
   shuffle(smoelen);
-  console.log(smoelen);
-  for (var i = 0; i < PHOTOAMOUNT ; i++){
+  for (let i = 0; i < smoelen.length &&  i < PHOTOAMOUNT ; i++){
     smoelenArray.unshift(smoelen[i]);
   }
 
-
-  for (var i = 0; i < smoelenArray.length; i++) {
-   var listItem = document.createElement('a');
-   listItem.innerHTML = smoelenArray[i][0];
-   listItem.classList.add("list-group-item");
-   listItem.classList.add('list-group-item-action');
-   listItem.addEventListener("click", onUserClickName);
-   listNames.appendChild(listItem);
-
-  }
-  for (var i = 0; i < smoelenArray.length; i++) {
+  for (let i = 0; i < smoelenArray.length; i++) {
     nameList.unshift({name: smoelenArray[i][0], correct: false, photo: smoelenArray[i][1]});
-  }
-  var listPhotos = document.getElementById('photoList');
-  shuffle(smoelenArray);
-  for (var i = 0; i < smoelenArray.length; i++) {
-   var listItem = document.createElement('img');
-   listItem.src = smoelenArray[i][1];
-   listItem.dataset.photo = smoelenArray[i][0];
-   listItem.classList.add("list-group-item");
-   listItem.classList.add('list-group-item-action');
-   listItem.classList.add('img-fluid');
-   listItem.style.maxHeight = "200px";
-   listItem.style.maxWidth = "200px";
-   listItem.style.minWidth= "0";
-   listItem.addEventListener("click", onUserClickPhoto);
-   listPhotos.appendChild(listItem);
+    listItem = document.createElement('a');
+    listItem.innerHTML = smoelenArray[i][0];
+    listItem.className = "list-group-item list-group-item-action";
+    listItem.addEventListener("click", onUserClick);
+    LISTNAMES.appendChild(listItem);
   }
 
+  shuffle(smoelenArray);
+  for (let i = 0; i < smoelenArray.length; i++) {
+    listItem = document.createElement('img');
+    listItem.src = smoelenArray[i][1];
+    listItem.dataset.photo = smoelenArray[i][0];
+    listItem.className = "listItem list-group-item list-group-item-action img-fluid";
+    listItem.addEventListener("click", onUserClick);
+    LISTPHOTOS.appendChild(listItem);
+  }
 }
 
-  function onUserClickName(){
+function onUserClick(){
+  selectedPhoto.className = "listItem list-group-item list-group-item-action";
   selectedName.className = "list-group-item list-group-item-action";
-  selectedName = this;
-  nameSelected = true;
+  (this.tagName == "IMG") ? (selectedPhoto = this, photoSelected = true, photoData = selectedPhoto.dataset.photo)
+  : (selectedName = this, nameSelected = true);
   this.classList.add('active');
-  onPlayerMove(photoData);
-  }
-
-  function onUserClickPhoto(){
-  selectedPhoto.className = "list-group-item list-group-item-action";
-  selectedPhoto = this;
-  photoSelected = true;
-  photoData = selectedPhoto.dataset.photo;
-  this.classList.add('active');
-  onPlayerMove(photoData);
-  }
-
-function onPlayerMove(photoData){
-  if(nameSelected && photoSelected){
-    totalMoves++;
-    onMatchTry(photoData);
-  }
+  onMatchTry(photoData);
 }
 
 function onMatchTry(photoData){
-  if (photoData == selectedName.innerHTML) {
-    correctMoves++;
-    for (var i = 0; i < nameList.length; i++) {
-      if (selectedName.innerHTML == nameList[i]['name']){
-        nameList[i]['correct'] = true;
-        console.log(JSON.stringify(nameList[i]));
+  if (nameSelected && photoSelected) {
+    totalMoves++;
+    if (photoData == selectedName.innerHTML) {
+      correctMoves++;
+      for (let i = 0; i < nameList.length; i++) {
+        if (selectedName.innerHTML == nameList[i]['name']){
+          nameList[i]['correct'] = true;
+        }
       }
+      selectedPhoto.classList.add('fade');
+      selectedName.classList.add('fade');
+      selectedName.removeEventListener("click", onUserClick);
+      selectedPhoto.removeEventListener("click", onUserClick);
+    }else{
+      selectedPhoto.className = "listItem list-group-item list-group-item-action";
+      selectedName.className = "list-group-item list-group-item-action";
     }
-
-    selectedPhoto.classList.add('fade');
-    selectedName.classList.add('fade');
-    selectedName.removeEventListener("click", onUserClickName);
-    selectedPhoto.removeEventListener("click", onUserClickPhoto);
-  }else{
-    selectedPhoto.className = "list-group-item list-group-item-action";
-    selectedName.className = "list-group-item list-group-item-action";
+   document.getElementById('scoreboard').innerHTML = correctMoves + " out of " + totalMoves;
+   selectedPhoto = "";
+   selectedName = "";
+   nameSelected = false;
+   photoSelected = false;
   }
-  document.getElementById('scoreboard').innerHTML = correctMoves + " out of " + totalMoves;
-  selectedPhoto = "";
-  selectedName = "";
-  nameSelected = false;
-  photoSelected = false;
 }
 
 function saveScore(){
   savePeople();
-  var dateNow = new Date();
-  var score = '200';
+  dateNow = new Date();
+  score = '200';
   if (!localStorage.getItem('scores')) {
     score = [{score: '200', date: dateNow}];
     localStorage.setItem('scores', JSON.stringify(score));
   }else {
-    var scoreboard = JSON.parse(localStorage.getItem('scores'));
+    scoreboard = JSON.parse(localStorage.getItem('scores'));
     scoreboard.unshift({score: score, date: dateNow});
     localStorage.setItem('scores', JSON.stringify(scoreboard));
   }
 }
 
 function savePeople(){
-  var people;
-    if (!localStorage.getItem('names')) {
-      people = [nameList];
-      localStorage.setItem('names', JSON.stringify(people));
-    }else {
-      var peoples = JSON.parse(localStorage.getItem('names'));
-      peoples.unshift(nameList);
-      localStorage.setItem('names', JSON.stringify(peoples));
-    }
+  (!localStorage.getItem('names')) ? (people = [nameList], localStorage.setItem('names', JSON.stringify(people)))
+  : (peoples = JSON.parse(localStorage.getItem('names')), peoples.unshift(nameList), localStorage.setItem('names', JSON.stringify(peoples)));
 }
 
 function TrainerTimer(duration, granularity){
@@ -198,17 +193,11 @@ TrainerTimer.prototype.start = function(){
 
   (function timer(){
     diff = that.duration - (((Date.now() - start) / 1000) | 0);
-    if (diff > 0){
-      setTimeout(timer, that.granularity);
+    (diff > 0) ? setTimeout(timer, that.granularity) : (diff = 0, that.running = false);
 
-    }else{
-      diff = 0;
-      that.running = false;
-    }
     obj = TrainerTimer.parse(diff);
     that.tickFtns.forEach(function(ftn) {
       ftn.call(this, obj.minutes, obj.seconds, obj.pureseconds);
-
     }, that);
 
   }());
